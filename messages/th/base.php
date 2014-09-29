c84dd16be7d58993bf6492619d993c1e1d7d72e7
Manop Kongoon <kongoon@hotmail.com>
2014-09-29 08:09:34 +0700

    Update base.php

--- messages/th/base.php
+++ messages/th/base.php
@@ -17,6 +17,6 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'Translation Manager' => '',
-  'Translations' => '',
+  'Translation Manager' => 'จัดการการแปลภาษา',
+  'Translations' => 'การแปลภาษา',
 );
