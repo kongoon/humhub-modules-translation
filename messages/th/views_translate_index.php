eaf57c13a3eda820bea7315ee59fdd66326c4e87
Manop Kongoon <kongoon@hotmail.com>
2014-09-29 08:11:10 +0700

    Update views_translate_index.php

--- messages/th/views_translate_index.php
+++ messages/th/views_translate_index.php
@@ -17,6 +17,6 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'Save' => '',
-  'Translation Editor' => '',
+  'Save' => 'บันทึก',
+  'Translation Editor' => 'ตัวช่วยเขียนการเปลี่ยนภาษา',
 );
